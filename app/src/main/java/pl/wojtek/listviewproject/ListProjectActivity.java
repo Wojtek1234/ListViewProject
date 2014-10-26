726838fc2378c6e1d12aa2bb1b21665e03bacf86
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-10-26 15:19:11 +0100

    Initial commit

---
+++ app/src/main/java/pl/wojtek/listviewproject/ListProjectActivity.java
@@ -0,0 +1,18 @@
+package pl.wojtek.listviewproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ListProjectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_project);
+    }
+
+
+}
