ff6c38de69537883e1a7baeaf74e16ffb757c787
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-10-26 16:11:07 +0100

    setUpList

---
+++ app/src/main/java/pl/wojtek/listviewproject/MyCustomListObject.java
@@ -0,0 +1,32 @@
+package pl.wojtek.listviewproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 2014-10-26.
+ */
+public class MyCustomListObject {
+
+
+
+    private String name;
+
+    public MyCustomListObject(String name) {
+        this.name = name;
+    }
+
+    public static List<MyCustomListObject> generateList(){
+        List<MyCustomListObject> result=new ArrayList<MyCustomListObject>();
+
+        for(int i=0;i<20;i++){
+            MyCustomListObject myCustomListObject=new MyCustomListObject("Name" +i);
+            result.add(myCustomListObject);
+        }
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/pl/wojtek/listviewproject/MyObjectAdapter.java
@@ -0,0 +1,35 @@
+package pl.wojtek.listviewproject;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by user on 2014-10-26.
+ */
+public class MyObjectAdapter extends ArrayAdapter<MyCustomListObject> {
+
+    private final List<MyCustomListObject> list;
+    private Activity context;
+
+    public MyObjectAdapter(Activity context, int resource, List<MyCustomListObject> objects) {
+        super(context, resource, objects);
+        this.context=context;
+        this.list=objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater=context.getLayoutInflater();
+        View view= layoutInflater.inflate(R.layout.item_row,null);
+
+        TextView textView=(TextView)view.findViewById(R.id.nameTextView);
+        textView.setText(this.list.get(position).getName());
+        return super.getView(position, convertView, parent);
+    }
+}
