05691d08ac0e00b2f4bd7dce114a920035f6b221
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-10-26 17:03:30 +0100

    clicker handled

--- app/src/main/java/pl/wojtek/listviewproject/ListProjectActivity.java
+++ app/src/main/java/pl/wojtek/listviewproject/ListProjectActivity.java
@@ -2,11 +2,15 @@ package pl.wojtek.listviewproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class ListProjectActivity extends Activity {
-
+    private ArrayAdapter<MyCustomListObject> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +20,16 @@ public class ListProjectActivity extends Activity {
 
     private void initListView() {
         ListView listView=(ListView)findViewById(R.id.listView);
+        this.adapter=new MyObjectAdapter(this,R.layout.item_row, MyCustomListObject.generateList());
+        listView.setAdapter( this.adapter);
 
-        listView.setAdapter(new MyObjectAdapter(this,R.layout.item_row, MyCustomListObject.generateList()));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    MyCustomListObject object=(MyCustomListObject)parent.getAdapter().getItem(position);
+                    Toast.makeText(ListProjectActivity.this,"Clicked"+object.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
